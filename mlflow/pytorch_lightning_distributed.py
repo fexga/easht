5342523013cd083e18300b4969453564bef0c287
felixgraf <felix.graf@campus.tu-berlin.de>
2025-02-17 11:09:35 +0100

    working mlflow dashboard

--- mlflow/pytorch_lightning_distributed.py
+++ mlflow/pytorch_lightning_distributed.py
@@ -8,6 +8,11 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 import optuna
 from optuna.trial import TrialState
+import mlflow
+import mlflow.pytorch
+
+mlflow.set_tracking_uri("http://mlflow:5000")
+mlflow.set_experiment("pytorch-lightning-distributed")
 
 BATCHSIZE = 128
 EPOCHS = 5
@@ -33,27 +38,39 @@ class Net(nn.Module):
         return self.layers(x)
 
 def objective(trial):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    train_dataset = MNIST(root="~/data", train=True, transform=transform, download=True)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCHSIZE, shuffle=True)  # Use DataLoader
+    with mlflow.start_run():
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        train_dataset = MNIST(root="~/data", train=True, transform=transform, download=True)
+        train_loader = DataLoader(dataset=train_dataset, batch_size=BATCHSIZE, shuffle=True)
 
-    model = Net(trial)
-    criterion = nn.CrossEntropyLoss()
-    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
-    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-    optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
+        model = Net(trial)
+        criterion = nn.CrossEntropyLoss()
+        optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+        lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+        optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
-    for epoch in range(EPO
-    CHS):
-        model.train()
-        for batch_idx, (data, target) in enumerate(train_loader):
-            optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, target)
-            loss.backward()
-            optimizer.step()
+        # Log parameters
+        mlflow.log_param("optimizer", optimizer_name)
+        mlflow.log_param("learning_rate", lr)
+        mlflow.log_param("n_layers", trial.params["n_layers"])
+        mlflow.log_param("dropout", trial.params["dropout"])
 
-    return loss.item()
+        for epoch in range(EPOCHS):
+            model.train()
+            for batch_idx, (data, target) in enumerate(train_loader):
+                optimizer.zero_grad()
+                output = model(data)
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()
+
+            # Log metrics for each epoch
+            mlflow.log_metric("loss", loss.item(), step=epoch)
+
+        # Log the model
+        mlflow.pytorch.log_model(model, "model")
+
+        return loss.item()
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="minimize")
